6f2984473e6e45c26118e87af5e6c23a78e86628
Stéphane Klein <stephaneklein221@gmail.com>
2015-06-25 16:56:46 +0200

    Choice question draft (1)

--- test-helpers.js
+++ test-helpers.js
@@ -1,34 +1,61 @@
 var assert = require('assert');
 var Ajv = require('ajv');
-var ajv = Ajv();
+var ajv = Ajv({ allErrors: true });
+
+function getValidator(schemaDir) {
+  var schema = require('./format/' + schemaDir + '/schema.json');
+
+  return ajv.compile(schema); // schema is cached by ajv
+}
+
+function getData(typeDir, dataFilePath) {
+  return require('./format/' + typeDir + '/examples/' + dataFilePath);
+}
+
+function validateAndNormalizeErrors(typeDir, dataFilePath, expectedErrors) {
+  var validate = getValidator(typeDir);
+  var data = getData(typeDir, dataFilePath);
+  var errors = {};
+
+  validate(data);
+
+  if (!validate.errors) {
+    validate.errors = [];
+  }
+
+  validate.errors.forEach(function (error) {
+    errors[error.dataPath] = errors[error.dataPath] || [];
+    errors[error.dataPath].push(error.message);
+  });
+
+  return errors;
+}
+
+function assertIsValid(typeDir, dataFilePath) {
+  var errors = validateAndNormalizeErrors(typeDir, dataFilePath);
+  assert.deepEqual(errors, [], 'Validation was not supposed to return any errors');
+}
+
+function assertHasErrors(typeDir, dataFilePath, expectedErrors) {
+  var errors = validateAndNormalizeErrors(typeDir, dataFilePath);
+  assert.deepEqual(errors, expectedErrors, 'Validation result does not match expectations');
+}
+
+function assertHasError(typeDir, dataFilePath, expectedError) {
+  var errors = validateAndNormalizeErrors(typeDir, dataFilePath);
+  var expectedPath = Object.keys(expectedError)[0];
+  var expectedMessage = expectedError[expectedPath];
+  var errorsAsString = '(errors: ' + JSON.stringify(errors) + ')';
+
+  assert(expectedPath in errors, 'No error at expected path ' + errorsAsString);
+  assert(
+    errors[expectedPath].indexOf(expectedMessage) > -1,
+    'The expected error ("' +  expectedMessage+ '") is missing ' + errorsAsString
+  );
+}
 
 module.exports = {
-  getValidator: function (schemaDir) {
-    var schema = require('./format/' + schemaDir + '/schema.json');
-
-    return ajv.compile(schema); // schema is cached by ajv
-  },
-  getData: function (typeDir, dataFilePath) {
-    return require('./format/' + typeDir + '/examples/' + dataFilePath);
-  },
-  assertValidate: function (typeDir, dataFilePath, expectedErrors) {
-    var validate = this.getValidator(typeDir);
-    var data = this.getData(typeDir, dataFilePath);
-    var errors;
-
-    validate(data);
-
-    if (!validate.errors) {
-      validate.errors = [];
-    }
-
-    errors = validate.errors.map(function (error) {
-      var wrapper = {};
-      wrapper[error.dataPath] = error.message;
-
-      return wrapper;
-    });
-
-    assert.deepEqual(errors, expectedErrors, 'Validation result does not match expectations');
-  }
+  assertIsValid: assertIsValid,
+  assertHasErrors: assertHasErrors,
+  assertHasError: assertHasError
 };
--- test/question/choice-test.js
+++ test/question/choice-test.js
@@ -1,60 +1,167 @@
 var helpers = require('./../../test-helpers');
 
-describe('A choice question', function () {
-  it('must be an object', function () {
-    helpers.assertValidate('question/choice', 'invalid/not-an-object.json', [
-      { '': 'should be object' }
-    ]);
-  });
-
-  it('must have a *choiceType* property', function () {
-    helpers.assertValidate('question/choice', 'invalid/no-choice-type-property.json', [
-      { '': 'properties choiceType, choices are required' }
-    ]);
-  });
-
-  it('must have a *choices* property', function () {
-    helpers.assertValidate('question/choice', 'invalid/no-choices-property.json', [
-      { '': 'properties choiceType, choices are required' }
-    ]);
-  });
-
-  describe('Its *choiceType* property', function () {
-    it('must be a mime type', function () {
-      helpers.assertValidate('question/choice', 'invalid/choice-type-is-not-a-mime.json', [
-        { '.choiceType': 'should match pattern \"^[^/]+/[^/]+$\"' }
-      ]);
-    });
-  });
-
-  describe('Its *choices* property', function () {
-    it('must be an array', function () {
-      helpers.assertValidate('question/choice', 'invalid/choices-is-not-an-array.json', [
-        { '.choices': 'should be array' }
-      ]);
-    });
-
-    it('must contain at least two choices', function () {
-      helpers.assertValidate('question/choice', 'invalid/under-two-choices.json', [
-        { '.choices': 'should NOT have less than 2 items' }
-      ]);
-    });
-
-    describe('Each choice', function () {
+describe('Choice type', function () {
+  describe('Structure', function () {
+    describe('A choice question', function () {
       it('must be an object', function () {
-        helpers.assertValidate('question/choice', 'invalid/no-choice-object.json', [
-          { '.choices[0]': 'should be object' }
-        ]);
+        helpers.assertHasError('question/choice', 'invalid/not-an-object.json', {
+          '': 'should be object'
+        });
       });
 
-      it('must have an "id" property', function () {
-        helpers.assertValidate('question/choice', 'invalid/no-choice-id.json', [
-          { '.choices[0]': 'properties id, text are required' }
-        ]);
+      it('must have an *id* property', function () {
+        helpers.assertHasError('question/choice', 'invalid/no-id-property.json', {
+          '': 'properties id, title, choices are required'
+        });
       });
 
-      it('must be unique', function () {
-        throw new Error('NOT IMPLEMENTED');
+      it('must have a *title* property', function () {
+        helpers.assertHasError('question/choice', 'invalid/no-title-property.json', {
+          '': 'properties id, title, choices are required'
+        });
+      });
+
+      it('must have a *choices* property', function () {
+        helpers.assertHasError('question/choice', 'invalid/no-choices-property.json', {
+          '': 'properties id, title, choices are required'
+        });
+      });
+
+      describe('The *id* property', function () {
+        it('must be a string', function () {
+          helpers.assertHasError('question/choice', 'invalid/id-property-is-not-a-string.json', {
+            '.id': 'should be string'
+          });
+        });
+      });
+
+      describe('The *title* property', function () {
+        it('must be a string', function () {
+          helpers.assertHasError('question/choice', 'invalid/title-property-is-not-a-string.json', {
+            '.title': 'should be string'
+          });
+        });
+      });
+
+      describe('The *choices* property', function () {
+        it('must be an array', function () {
+          helpers.assertHasError('question/choice', 'invalid/choices-is-not-an-array.json', {
+            '.choices': 'should be array'
+          });
+        });
+
+        it('must contain at least two choices', function () {
+          helpers.assertHasError('question/choice', 'invalid/under-two-choices.json', {
+            '.choices': 'should NOT have less than 2 items'
+          });
+        });
+
+        describe('Each choice', function () {
+          it('must be an object', function () {
+            helpers.assertHasError('question/choice', 'invalid/no-choice-object.json', {
+              '.choices[0]': 'should be object'
+            });
+          });
+
+          it('must be unique', function () {
+            helpers.assertHasError('question/choice', 'invalid/duplicate-choices.json', {
+              '.choices': 'items ## 0 and 2 are duplicate'
+            });
+          });
+
+          it('must have an *id* property', function () {
+            helpers.assertHasError('question/choice', 'invalid/no-choice-id.json', {
+              '.choices[0]': 'properties id, type, data are required'
+            });
+          });
+
+          it('must have a *type* property', function () {
+            helpers.assertHasError('question/choice', 'invalid/no-choice-type.json', {
+              '.choices[0]': 'properties id, type, data are required'
+            });
+          });
+
+          it('must have either a *data* or an *url* property', function () {
+            helpers.assertHasError('question/choice', 'invalid/no-choice-data-nor-url.json', {
+              '.choices[0]': 'properties id, type, data are required'
+            });
+            helpers.assertHasError('question/choice', 'invalid/no-choice-data-nor-url.json', {
+              '.choices[0]': 'properties id, type, url are required'
+            });
+          });
+
+          describe('The *id* property', function () {
+            it('must be a string', function () {
+              helpers.assertHasError('question/choice', 'invalid/choice-id-is-not-a-string.json', {
+                '.choices[1].id': 'should be string'
+              });
+            });
+          });
+
+          describe('The *type* property', function () {
+            it('must be a string', function () {
+              helpers.assertHasError('question/choice', 'invalid/choice-type-is-not-a-string.json', {
+                '.choices[0].type': 'should be string'
+              });
+            });
+
+            it('must hold a MIME type', function () {
+              helpers.assertHasError('question/choice', 'invalid/choice-type-is-not-a-mime.json', {
+                '.choices[0].type': 'should match pattern \"^[^/]+/[^/]+$\"'
+              });
+            });
+          });
+
+          describe('The *url* property', function () {
+            it('must be a string', function () {
+              helpers.assertHasError('question/choice', 'invalid/choice-url-is-not-a-string.json', {
+                '.choices[1].url': 'should be string'
+              });
+            });
+
+            it('must hold an URL', function () {
+              helpers.assertHasError('question/choice', 'invalid/choice-url-is-not-valid.json', {
+                '.choices[1].url': 'should match format uri'
+              });
+            });
+          });
+
+          describe('The *data* property', function () {
+            it('must be a string', function () {
+              helpers.assertHasError('question/choice', 'invalid/choice-data-is-not-a-string.json', {
+                '.choices[0].data': 'should be string'
+              });
+            });
+
+            it('may have an associated *encoding* property', function () {
+              helpers.assertHasError('question/choice', 'invalid/choice-encoding-with-url.json', {
+                ".choices[0]['encoding']": 'additional properties NOT allowed'
+              });
+            });
+
+            describe('The *encoding* property', function () {
+              it('must be a string', function () {
+                helpers.assertHasError('question/choice', 'invalid/choice-encoding-is-not-a-string.json', {
+                  '.choices[0].encoding': 'should be string'
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
+  describe('Examples', function () {
+    var examples = [
+      'true-false',
+      'distant-medias',
+      'embedded-medias'
+    ];
+
+    examples.forEach(function (example) {
+      it(example + '.json', function () {
+        helpers.assertIsValid('question/choice', 'valid/' + example + '.json', []);
       });
     });
   });
