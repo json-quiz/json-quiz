7ede298eaad9a45e766d4df26d444d9cdc09d584
Stéphane Klein <stephaneklein221@gmail.com>
2015-09-29 11:46:08 +0200

    Add schema dir from build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-echo "Regenerating spec and data directories..."
+echo "Regenerating directories from ./build..."
 
 cd `dirname $0`
-rm -rf _spec _data
+rm -rf _spec _data schemas
 cp -R build/_spec .
 cp -R build/_data .
+cp -R build/schemas .
