15da51bdac43dcaf68811272ef22ea4d5d54ddc8
Stéphane Klein <stephaneklein221@gmail.com>
2016-02-12 11:34:03 +0100

    Remove array wrapper from short answer

--- test/answer/short-answer-test.js
+++ test/answer/short-answer-test.js
@@ -4,43 +4,26 @@ describe('Short answer', function () {
   describe('Schema', function () {
 
     describe('A short answer', function () {
-      it('must be an array', function () {
-        assert.hasError('short-answer-is-not-an-array', {
-          '': 'should be array'
+      it('must be an object', function () {
+        assert.hasError('short-answer-is-not-an-object', {
+          '': 'should be object'
         });
       });
-      describe('Each answer', function () {
-        it('must be an object', function () {
-          assert.hasError('short-answer-answer-is-not-an-object', {
-            '[0]': 'should be object'
-          });
+      it('must have a *text* property', function () {
+        assert.hasError('short-answer-no-text', {
+          '.text': 'property .text is required'
         });
-        it('must not have less than one item', function () {
-          assert.hasError('short-answer-less-than-1-item', {
-            '': 'should NOT have less than 1 items'
-          });
-        });
-        it('must not have more than one item', function () {
-          assert.hasError('short-answer-more-than-1-item', {
-            '': 'should NOT have more than 1 items'
-          });
-        });
-        it('must have a *text* property', function () {
-          assert.hasError('short-answer-no-text', {
-            '[0].text': 'property .text is required'
-          });
-        });
-        describe('The *text* property', function () {
-          it('must be a string', function () {
-            assert.hasError('short-answer-text-is-not-a-string', {
-              '[0].text': 'should be string'
-            });
+      });
+      describe('The *text* property', function () {
+        it('must be a string', function () {
+          assert.hasError('short-answer-text-is-not-a-string', {
+            '.text': 'should be string'
           });
         });
       });
     });
   });
-  
+
   describe('Examples', function () {
     assert.areValid([
       'short-answer'
